f00cb94cf40d5e5bfd412d12710fc1c637286d2a
DavidMoralesVargas <dacanad3@gmail.com>
2026-06-16 21:16:50 -0500

    Subiendo cambios en una nueva rama

--- Chat.Backend/Hexagonal.Application/Puertos/Salida/IUsuariosRepository.cs
+++ Chat.Backend/Hexagonal.Application/Puertos/Salida/IUsuariosRepository.cs
@@ -5,7 +5,7 @@ namespace Hexagonal.Application.Persistencia
 {
     public interface IUsuariosRepository
     {
-        bool BuscarUsuario(string email);
+        bool BuscarUsuario(string email); //cambio
         bool VerificarContrasena(string password);
         Usuario RegistrarUsuario(string usuario, string email, string contrasena);
     }
